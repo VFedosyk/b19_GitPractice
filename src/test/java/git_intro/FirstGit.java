80f42e7b189bb2e66c8a78a6389a7d7af8237ae4
VFedosyk <volodymyr.fedosyk@gmail.com>
2024-03-20 17:17:01 -0500

    second

--- src/test/java/git_intro/FirstGit.java
+++ src/test/java/git_intro/FirstGit.java
@@ -4,5 +4,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("My first git practice!");
         System.out.println("First Git practice command");
+        System.out.println("Hello");
     }
 }
